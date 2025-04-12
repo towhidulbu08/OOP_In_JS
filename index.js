2a6fe96be48251865f39402e1d0188dc4b587dac
towhidulbu08 <towhidulislam26121997@gmail.com>
2025-04-12 17:25:05 +0600

    value vs reference types

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-//NOTE: Functions Are Objects:
+//*Value vs Reference Types:
 
-// Functions are objects in JavaScript, and they have properties and methods just like any other object.
+// Value types are stored in the stack and are copied when assigned to a new variable.
+// Reference types are stored in the heap and are referenced when assigned to a new variable.
 
-function Circle(radius) {
-  this.radius = radius;
+// Value types include: undefined, null, boolean, number, string, bigint, symbol
+// Reference types include: object, array, function, date, regex, etc.
+// Value types are immutable, meaning they cannot be changed once created.
+// Reference types are mutable, meaning they can be changed after creation.
 
-  this.draw = function () {
-    console.log("draw");
-  };
+let x = 10; // Value type
+let y = x; // y is a copy of x
+
+x = 20; // x is changed to 20
+
+let a = { value: 10 }; // Reference type
+let b = a; // b is a reference to a
+
+a.value = 20; // a is changed to 20
+
+//NOTE: object is not stored in variable a or b, but stored somewhere else in memory called heap and the address of that memory is stored inside a and b. both a and b are pointing to the same object in memory. So when we change the value of a, it also changes the value of b because they are both pointing to the same object in memory.
+
+//*Conclusion: Primitive are copied by value and objects are copied by reference(memory reference).
+
+//let number = 10;
+
+let obj = { value: 10 };
+
+function increase(obj) {
+  obj.value++;
+  //console.log(number); // 11
 }
-Circle.call({}, 1); // This expression is exactly the same as on the line 13, when we use new keyword, this new keyword  will internally create an empty object and pass it as the first argument to the call method and this empty object will determine the context for this. So this will reference to this empty object .
 
-Circle.apply({}, [1]); // the difference between call and apply is that in call we pass the arguments separately, but in apply we pass the arguments as an array.
-const another = new Circle(1);
+//increase(number);
+// when we call increase function and pass this number variable its value is copied into function parameter number and  function parameter are local variables. So parameter number are completely independent of this number variable.  they both are different variables. So when we increase the value of number inside the function it does not affect the value of number outside the function. So when we log the value of number outside the function it will still be 10.
+
+//console.log(number);
+
+increase(obj);
+console.log(obj);
